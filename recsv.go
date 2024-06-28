9746a1ff7cf457eb79936c7cda067015fbb41045
Wing <24888148+WingGithub@users.noreply.github.com>
2024-06-28 16:20:13 -0700

    Handle command line input and output file sepcification.

--- recsv.go
+++ recsv.go
@@ -9,7 +9,25 @@ import (
 )
 
 func main() {
-	reader := csv.NewReader(os.Stdin)
+	inFile := os.Stdin
+	outFile := os.Stdout
+	argc := len(os.Args)
+	var err error
+	if argc > 1 {
+		inFile, err = os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer inFile.Close()
+	}
+	if argc > 2 {
+		outFile, err = os.Create(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer outFile.Close()
+	}
+	reader := csv.NewReader(inFile)
 
 	// ReadAll reads all the records from the CSV file
 	// and Returns them as slice of slices of string
@@ -30,7 +48,7 @@ func main() {
 		return eq
 	})
 
-	w := csv.NewWriter(os.Stdout)
+	w := csv.NewWriter(outFile)
 	// Loop to iterate through
 	// and write out each of the string slice
 	for _, eachrecord := range records {
